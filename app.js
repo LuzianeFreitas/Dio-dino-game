47b5a55fd3ebc373eeb0dc292d3d5d5f4cb2aa90
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-12 13:13:07 -0300

    Added function createdCactus

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJump = false;
+
 
 function handleKeyUp(event){
     // Verifica se a tecla pressionada é o space
     if(event.keyCode === 32) {
-        jump();
+        if(!isJump){
+            jump();
+        }
+        
     }
 }
 
 function jump() {
     let position = 0;
-
+    isJump = true;
     let upInterval = setInterval( () => {
         if (position >= 150){
             // Limpando o intervalo
@@ -18,9 +25,11 @@ function jump() {
             let downInterval = setInterval( () => {
                 if(position <= 0) {
                     clearInterval(downInterval);
+                    isJump = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
+                    
                 }
                 
             }, 20);
@@ -33,5 +42,30 @@ function jump() {
     }, 20);
 }
 
+function createdCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        // Verifica se o cactu saiu da tela se sim remove ele se não continua o movimento
+        if(cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    // Recursividade para gerar mais cactus 
+    setTimeout(createdCactus, randomTime);
+}
+
+createdCactus();
 // Keyup captura uma tecla apertada
 document.addEventListener('keyup', handleKeyUp);
